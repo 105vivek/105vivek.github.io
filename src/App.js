d0bdf87eae5938c0725a10a615b39b4ad49a5e53
Vivek Yadav <105vivekyadav@gmail.com>
2023-12-12 23:27:11 +0530

    github_stat

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 // import "style.css";
 import Contact from "./components/Contact/contact";
 import Footer from "./components/Footer/footer";
+import Github from "./components/Github/github";
 import Intro from "./components/Intro/intro";
 import Navbar from "./components/Navbar/navbar";
 import Skills from "./components/Skills/skills";
@@ -13,6 +14,7 @@ function App() {
       <Skills />
       <Works />
       <Contact />
+      <Github />
       <Footer />
     </div>
   );
---
+++ src/components/Github/github.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Github = () => {
+  return (
+    <GitHubCalender
+      style={{ padding: "200px" }}
+      id="calender"
+      className="react-activity-calendar"
+      username="105vivek"
+    />
+  );
+};
+
+export default Github;
